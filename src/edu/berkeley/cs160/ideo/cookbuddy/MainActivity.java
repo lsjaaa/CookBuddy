339c13f80b8aa5e76f1dd5e926402f6a75bdc1e8
iamadawra <aayushgopaldawra@gmail.com>
2014-04-03 18:36:08 -0700

    Initial commit

---
+++ src/edu/berkeley/cs160/ideo/cookbuddy/MainActivity.java
@@ -0,0 +1,27 @@
+package edu.berkeley.cs160.ideo.cookbuddy;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.Window;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		//The following line removes the title bar and hence, better UI
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		
+		// And after removing the title bar, set content view
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
